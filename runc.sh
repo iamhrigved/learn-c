3aa9b79a61b5590cae42ae4ac7710eb1639dbce1
Hrigved <iamhrigved@gmail.com>
2024-09-25 18:30:02 +0530

    Update runc.sh file

--- runc.sh
+++ runc.sh
@@ -1,17 +1,12 @@
+#!/bin/bash
+
 # run `source runc.sh` and then use this funcion anywhere
 runc() {
 	local file_location=$1
+    local file_type=$(echo $file_location | sed "s/.*\.\(.*\)/\1/" | sed "s/p/+/g") # `c` or `cpp`
 	local file_name=${1:t}
 	local file_directory=${1:h}
 	local exe_file_name=${file_name:r}
-	local is_local=""
-	local exe_file_location=""
-
-	if [[ $file_directory == "." ]]; then 
-		is_local=true
-	else
-		is_local=false
-	fi
 
     # if the file compiles without any errors this will run
     local run() {
@@ -23,8 +18,9 @@ runc() {
 
             mv ./a.out ./executables/$exe_file_name
             echo "" # printing new lines
-            ./executables/$exe_file_name
+            ./executables/$exe_file_name $@
             echo ""
     }
-    g++ $file_location && run
+    echo "g++ $file_location -x $file_type && run $@"
+    g++ $file_location -x $file_type && run $@
 }
